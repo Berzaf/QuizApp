281337e7f1f2b27d03adaf13e8fc692b948e8032
Berzaf <berzafastroo2012@gmail.com>
2021-06-18 21:33:23 -0500

    Fetch API Questions from Open API

--- game.js
+++ game.js
@@ -9,7 +9,7 @@ let score = 0;
 let questionCounter = 0;
 let availableQuesions = [];
 
-let questions = [
+/*let questions = [
     {
         question: 'Inside which HTML element do we put the JavaScript??',
         choice1: '<script>',
@@ -36,7 +36,41 @@ let questions = [
         answer: 4,
     },
 ];
+*/
 
+let questions = [];
+
+fetch(
+    'https://opentdb.com/api.php?amount=10&category=9&difficulty=easy&type=multiple'
+)
+    .then((res) => {
+        return res.json();
+    })
+    .then((loadedQuestions) => {
+        questions = loadedQuestions.results.map((loadedQuestion) => {
+            const formattedQuestion = {
+                question: loadedQuestion.question,
+            };
+
+            const answerChoices = [...loadedQuestion.incorrect_answers];
+            formattedQuestion.answer = Math.floor(Math.random() * 4) + 1;
+            answerChoices.splice(
+                formattedQuestion.answer - 1,
+                0,
+                loadedQuestion.correct_answer
+            );
+
+            answerChoices.forEach((choice, index) => {
+                formattedQuestion['choice' + (index + 1)] = choice;
+            });
+
+            return formattedQuestion;
+        });
+        startGame();
+    })
+    .catch((err) => {
+        console.error(err);
+    });
 
 //CONSTANTS
 const CORRECT_BONUS = 10;
